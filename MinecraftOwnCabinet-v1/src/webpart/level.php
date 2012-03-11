57e502a5d999e3e617da006051acc115dd0f2f04
Andrey Sinitsyn <andrey.sin98@gmail.com>
2012-03-11 16:05:43 +0400

    Ыыыыы.... Облажался.

--- MinecraftOwnCabinet-v1/src/webpart/level.php
+++
@@ -1,9 +0,0 @@
-<?php
-if ($db_level > '0') {
-   print '<div class="otstup"><b>Уровень:</b> '.$db_level.'</div>';
-}
-
-if (!$db_level) {
-   print '<div class="otstup"><b>Уровень:</b> '.Нет.'</div>';
-}
-?>
